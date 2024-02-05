0e0bbfad26ec8dd2d443c9baaeb21f94b7691b80
navjotNSK <navjotsingh.uiet@gmail.com>
2024-02-05 12:40:05 +0530

    Main method and pom added

---
+++ insight-zone-BE-v1/src/main/java/com/InsightZone/insight/zone/v1/InsightZoneV1Application.java
@@ -0,0 +1,13 @@
+package com.InsightZone.insight.zone.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightZoneV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightZoneV1Application.class, args);
+	}
+
+}
---
+++ insight-zone-BE-v1/src/test/java/com/InsightZone/insight/zone/v1/InsightZoneV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InsightZone.insight.zone.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightZoneV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
